40fac6759554f763c862be2bd3c1cc436e9c3ca5
it@sourx.com <abdous100@gmail.com>
2025-06-27 12:15:37 +0100

    J'ai commenté la ligne import BuyerDashboard from './acheteur' car le fichier acheteur.tsx n'existe pas dans pages/dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -4,7 +4,7 @@ import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import { CircularProgress, Box } from '@mui/material';
 import AdminDashboard from './admin';
-import BuyerDashboard from './acheteur';
+//import BuyerDashboard from './acheteur';
 import FarmerDashboard from './agriculteur';
 
 const Dashboard = () => {
@@ -34,8 +34,8 @@ const Dashboard = () => {
     case 'ADMIN':
     case 'SUPERADMIN':
       return <AdminDashboard />;
-    case 'ACHETEUR':
-      return <BuyerDashboard />;
+    //case 'ACHETEUR':
+    //  return <BuyerDashboard />;
     case 'AGRICULTEUR':
       return <FarmerDashboard />;
     default:
